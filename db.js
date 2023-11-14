9f072311c88646d82bf07c7191e4531cc366fea8
ThiagoAciole <thiagoaciole7@gmail.com>
2023-11-13 21:43:38 -0300

    ajustes

--- db.js
+++ db.js
@@ -1,12 +1,26 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
-const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.MONGODB_CONNECT_URI)
-        console.log("Connect to MongoDB successfully")
-    } catch (error) {
-        console.log("Connect failed " + error.message )
-    }
-}
+const dbURI = 'mongodb+srv://thiagoaciole7:NHg1ErJArHUQiWuY@blog.h6hsmfs.mongodb.net/?retryWrites=true&w=majority';
+
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+mongoose.connection.on('connected', () => {
+  console.log(`Mongoose connected to ${dbURI}`);
+});
+
+mongoose.connection.on('error', (err) => {
+  console.error(`Mongoose connection error: ${err}`);
+});
+
+mongoose.connection.on('disconnected', () => {
+  console.log('Mongoose disconnected');
+});
+
+process.on('SIGINT', () => {
+  mongoose.connection.close(() => {
+    console.log('Mongoose disconnected through app termination');
+    process.exit(0);
+  });
+});
 
 module.exports = mongoose;
